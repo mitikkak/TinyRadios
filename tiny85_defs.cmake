0ea594c9abf7dd7185cec0b91ada0662ca32817a
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2017-10-26 13:26:34 +0300

    forgot tiny85_defs.cmake form prev revision. alternative path for tiny85.

---
+++ tiny85_defs.cmake
@@ -0,0 +1,2 @@
+
+add_definitions(-c -g -Os -w -std=gnu++11 -fpermissive -fno-exceptions -ffunction-sections -fdata-sections -fno-threadsafe-statics -MMD -flto -mmcu=attiny85 -DF_CPU=8000000L -DARDUINO=10612 -DARDUINO_attiny -DARDUINO_ARCH_AVR)
