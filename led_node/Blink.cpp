90960bb2d471fa08492cb21886c0bb4dbbb1cabf
Mikko <mikko.tikkakoski@gmail.com>
2017-10-28 20:15:28 +0300

    class Blink added

--- led_node/Blink.cpp
+++ led_node/Blink.cpp
@@ -11,3 +11,31 @@ void doBlink(const int rounds, const int delay_)
     delay(delay_);
   }
 }
+void Blink::swapState()
+{
+    state = (state == HIGH) ? LOW : HIGH;
+    digitalWrite(ledPin, state);
+}
+Blink::Blink(const int rounds_, const TIME delay_)
+: rounds(rounds_), delay(delay_), startTime(0), prevTime(0), endTime(0), state(LOW)
+{
+}
+void Blink::begin(TIME const now)
+{
+    startTime = now;
+    endTime = now + (rounds * delay);
+    digitalWrite(ledPin, state);
+}
+void Blink::update(TIME const now)
+{
+    if (now >= endTime)
+    {
+        if (state == LOW) {swapState();}
+        return;
+    }
+    else if (now >= (prevTime+delay))
+    {
+        swapState();
+        prevTime = now;
+    }
+}
--- led_node/Blink.h
+++ led_node/Blink.h
@@ -5,4 +5,19 @@
 
 void doBlink(const int rounds, const int delay_);
 
+class Blink
+{
+    const int rounds;
+    const TIME delay;
+    TIME startTime;
+    TIME prevTime;
+    TIME endTime;
+    int state;
+    void swapState();
+public:
+    Blink(const int rounds_, const TIME delay_);
+    void begin(TIME const now);
+    void update(TIME const now);
+};
+
 
