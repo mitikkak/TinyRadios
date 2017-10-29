19976cdcba0f98fe530cebb89ad47a0eb8421d81
Mikko <mikko.tikkakoski@gmail.com>
2017-10-29 12:50:00 +0200

    led_node: added NODE_ALWAYS_ON option. Using the Blink class for blinking when connected. Timeouts shortened.

--- led_node/Loop.cpp
+++ led_node/Loop.cpp
@@ -21,10 +21,10 @@ Ping getPingRequest()
   radio.startListening();
   bool timeout = false;
   Ping req(0,0);
-  #if 1
+  #if 0
   TIME const started_waiting_at = micros();
   while ( !radio.available() ){ // While nothing is received
-    if (micros() - started_waiting_at > 500000 ){ // If waited longer than 200ms, indicate timeout and exit while loop
+    if (micros() - started_waiting_at > 10000 ){ // If waited longer than 200ms, indicate timeout and exit while loop
       //_SERIAL.println("timeout");
       timeout = true;
       timeouts++;
@@ -44,41 +44,51 @@ Ping getPingRequest()
 TIME timePrev = 0;
 bool connectionEstablished = false;
 TIME const CONNECTION_WAIT_PERIOD = 30000;
-
+Blink blinker(5, 300);
+#ifndef NODE_ALWAYS_ON
 void loop() {
-#if 0
-TIME const timeNow = millis();
-if (timeNow - timePrev < 10) { return; }
-timePrev= timeNow;
-#endif
-Ping const req = getPingRequest();
-if (req.header.msgId == PING_REQUEST)
-{
-  if (!connectionEstablished)
-  {
-    doBlink(3, 300);
-  }
-  connectionEstablished = true;
-  digitalWrite(ledPin, HIGH);
-  reqs++;
-  for (unsigned i = 0; i < 10; i++)
-  {
-    sendPingResponse(req);
-  }
-}
 
-if (millis() - prevLog >= CONNECTION_WAIT_PERIOD)
-{
-  prevLog = millis();
-  _SERIAL.print(prevLog);
-  _SERIAL.print(", reqs: "); _SERIAL.print(reqs);
-  _SERIAL.print(", timeouts: "); _SERIAL.println(timeouts);
-  if (!reqs)
-  {
-    connectionEstablished = false;
-    digitalWrite(ledPin, HIGH);
-  }
-  reqs = 0;
-  timeouts = 0;
-}                     // wait for a second
+    TIME const timeNow = millis();
+    #if 0
+    if (timeNow - timePrev < 10) { return; }
+    timePrev= timeNow;
+    #endif
+    Ping const req = getPingRequest();
+    if (req.header.msgId == PING_REQUEST)
+    {
+      if (!connectionEstablished)
+      {
+          blinker.begin(timeNow);
+//        doBlink(3, 300);
+      }
+      blinker.update(timeNow);
+      connectionEstablished = true;
+      //digitalWrite(ledPin, HIGH);
+      reqs++;
+      for (unsigned i = 0; i < 1; i++)
+      {
+        sendPingResponse(req);
+      }
+    }
+#if 0
+    if (millis() - prevLog >= CONNECTION_WAIT_PERIOD)
+    {
+      prevLog = millis();
+      _SERIAL.print(prevLog);
+      _SERIAL.print(", reqs: "); _SERIAL.print(reqs);
+      _SERIAL.print(", timeouts: "); _SERIAL.println(timeouts);
+      if (!reqs)
+      {
+        connectionEstablished = false;
+        digitalWrite(ledPin, HIGH);
+      }
+      reqs = 0;
+      timeouts = 0;
+    }                     // wait for a second
+#endif
 }
+#else
+void loop() {
+
+}
+#endif
--- led_node/Setup.cpp
+++ led_node/Setup.cpp
@@ -2,6 +2,7 @@
 #include "Components.h"
 #include "Blink.h"
 
+#ifndef NODE_ALWAYS_ON
 void setup() {
   _SERIAL.begin(9600);
   _SERIAL.println("begin");
@@ -10,8 +11,15 @@ void setup() {
   radio.setRetries(15,15); // Max delay between retries & number of retries
   radio.openWritingPipe(remote_address); // Write to device address '2Node'
   radio.openReadingPipe(1,my_address); // Read on pipe 1 for device address '1Node'
-  //radio.setPALevel(RF24_PA_LOW);
+  radio.setPALevel(RF24_PA_LOW);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  doBlink(3, 300);
+  //doBlink(3, 300);
 }
+#else
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+}
+#endif
